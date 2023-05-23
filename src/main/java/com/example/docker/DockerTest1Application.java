e6099e2d8d076b37013a088c2dc8c84bc2ea3697
Ritesh verma <53870926+riteshverma009@users.noreply.github.com>
2023-05-23 12:27:37 +0530

    Update DockerTest1Application.java

--- src/main/java/com/example/docker/DockerTest1Application.java
+++ src/main/java/com/example/docker/DockerTest1Application.java
@@ -11,7 +11,7 @@ public class DockerTest1Application {
 
 	@GetMapping("/test")
 	public String getMesage() {
-		return "suntech2025";
+		return "suntech2026";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(DockerTest1Application.class, args);
